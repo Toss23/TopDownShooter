6eccf49694a8d565dcb8bd22bad6e15fc4666994
Alexander <95612872+Toss23@users.noreply.github.com>
2024-08-04 12:28:43 +0300

    Camera fix

--- Assets/Game/Scripts/Context/CameraControl.cs
+++ Assets/Game/Scripts/Context/CameraControl.cs
@@ -3,12 +3,22 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class CameraControl : MonoBehaviour, IInitable, IUpdatable
 {
+    private const int MAP_WEDTH = 40;
+    private const int MAP_HEIGHT = 30;
+
     private Camera _camera;
     private Player _player;
+
+    private Vector2 _screenSize;
+    private Vector2 _cameraSize;
+    private Vector2 _borders;
 
     public void PreInit(IContext context)
     {
         _camera = GetComponent<Camera>();
+        _screenSize = new Vector2(Screen.width, Screen.height);
+        _cameraSize = new Vector2(_screenSize.x / _screenSize.y * _camera.orthographicSize, _camera.orthographicSize);
+        _borders = new Vector2(MAP_WEDTH / 2 - _cameraSize.x, MAP_HEIGHT / 2 - _cameraSize.y);
     }
 
     public void Init(IContext context)
@@ -19,7 +29,7 @@ public class CameraControl : MonoBehaviour, IInitable, IUpdatable
     public void UpdateGame(float deltaTime)
     {
         Vector2 position = _player.Movement.Position;
-        position = new Vector3(Mathf.Clamp(position.x, -9.75f, 9.75f), Mathf.Clamp(position.y, -7.25f, 7.25f), -10);
+        position = new Vector3(Mathf.Clamp(position.x, -_borders.x, _borders.x), Mathf.Clamp(position.y, -_borders.y, _borders.y), -10);
         _camera.transform.position = position;
     }
 
